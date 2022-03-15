2438f590c5d1a8ad7adfa16046383b4850704f8e
beatriznegreiros <beatriznegreiros@outlook.com>
2022-03-16 00:18:50 +0100

    inc. fnt size intro text

--- contact-form.php
+++ contact-form.php
@@ -1,10 +1,14 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "ricardovobarros@live.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+<?php 
+if ($_POST){
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$message = $_POST["message"];
+	$formcontent="From: $name \n Message: $message";
+	$recipient = "ricardovobarros@live.com";
+	$subject = "Contact Form";
+	$mailheader = "From: $email \r\n";
+	mail($recipient, $subject, $formcontent, $mailheader): bool
+	echo "Thank You!";
+}
+else {echo "<p> Somethinng went wrong</p>"}
 ?>
